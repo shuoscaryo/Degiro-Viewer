36ebee091c158ecf419fe060dbde8cf7adfb8b77
shuoscaryo <shuoscaryo@hgmail.com>
2026-05-15 20:08:14 +0200

    some input csv formatting

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-const DATE_HEADER = "Fecha";
-const DATE_VALUE_HEADER = "Fecha valor";
-const HOUR_HEADER = "Hora";
-const MERGED_DATE_VALUE_HEADER = "Date";
+// Degiro CSV header strings
+const IN_CSV_HEADER_FECHA = "Fecha";
+const IN_CSV_HEADER_HORA = "Hora";
+const IN_CSV_HEADER_FECHA_VALOR = "Fecha valor";
+const IN_CSV_HEADER_PRODUCTO = "Producto";
+const IN_CSV_HEADER_ISIN = "ISIN";
+const IN_CSV_HEADER_DESCRIPCION = "Descripción";
+const IN_CSV_HEADER_TIPO = "Tipo";
+const IN_CSV_HEADER_VARIACION = "Variación";
+const IN_CSV_HEADER_COL_9 = "col 9";
+const IN_CSV_HEADER_SALDO = "Saldo";
+const IN_CSV_HEADER_COL_11 = "col 11";
+const IN_CSV_HEADER_ID_ORDEN = "ID Orden";
+
+// Parsed Headers for different kinds of operations
+// All
+const HEADER_DATE = "date";
+const HEADER_TRANSACTION_TYPE = "type";
+const HEADER_MARKED_TAG = "marked";
+// buy/sell operation
+const HEADER_STOCK_NAME = "stock name";
+const HEADER_SHARE_COUNT = "share count";
+const HEADER_ENTRY_PRICE = "entry price";
+const HEADER_CURRENCY = "currency";
+const HEADER_ISIN = "ISIN";
+// fee
+const HEADER_AMOUNT = "amount";
 
 let result = [];
 
@@ -50,43 +73,127 @@ async function handler_fileUpload(event)
 {
   const file = event.target.files[0];
   const text = await file.text();
-  const csv = CSV_parse(text);
-  result = CSV_formatDate(csv);
-  console.table(result);
-}
-
-function CSV_formatDate(csv)
-{
-  // For each row, pick the date headers and merge them into one, then append the rest (DATE_HEADER is only there to remove it)
-  return csv.map((row) => {
-    const { [DATE_HEADER]: _, [HOUR_HEADER]: hour, [DATE_VALUE_HEADER]: valueDate, ...rest } = row;
-
-    const [day, month, year] = valueDate.split("-").map(Number);
-    const [hours = 0, minutes = 0] = (hour || "").split(":").map(Number);
-    const date = new Date(year, month - 1, day, hours, minutes);
-
-    return {
-      [MERGED_DATE_VALUE_HEADER]: date,
-      ...rest
-    };
-  });
+  let csv = CSV_parse(text);
+  csv = CSV_formatDate(csv);
+  csv = CSV_dropCol(csv, "ID Orden");
+  csv = CSV_formatOperations(csv);
+  console.log(`Marked after format operations ${csv.filter(row => row[HEADER_MARKED_TAG]).length}/ ${csv.length}`);
+  csv = CSV_formatFees(csv);
+  console.log(`Marked after format fees ${csv.filter(row => row[HEADER_MARKED_TAG]).length} / ${csv.length}`);
+  
 }
 
 function CSV_parse(text)
 {
+  // Parses the input file as csv.
+  // Undefined columns are renamed to "col ${index + 1}"
   const parsed = Papa.parse(text, {
     header: true,
     skipEmptyLines: true,
     transformHeader(header, index) {
       const name = header.trim();
-      return name || `Column ${index + 1}`;
+      return name || `col ${index + 1}`;
     }
   });
-
   if (parsed.errors.length)
     throw new Error(parsed.errors[0].message);
-
   return parsed.data;
+}
+
+function CSV_formatDate(/*const*/ csv)
+{
+  // For each row, pick the date headers and merge them into one column of Date type
+  return csv.map((row) => {
+    // Select elements from row
+    const { [IN_CSV_HEADER_FECHA]: _, [IN_CSV_HEADER_HORA]: hour, [IN_CSV_HEADER_FECHA_VALOR]: valueDate, ...rest } = row;
+    // Get the date variables and store them in a Date object
+    const [day, month, year] = valueDate.split("-").map(Number);
+    const [hours = 0, minutes = 0] = (hour || "").split(":").map(Number);
+    const date = new Date(year, month - 1, day, hours, minutes);
+    // return a new row with the new Date field and the used ones removed
+    return {
+      [HEADER_DATE]: date,
+      ...rest
+    };
+  });
+}
+
+function CSV_dropCol(/*const*/ csv, col) {
+  // Removes a column from the csv
+  // If col doesn't exist it doesn't do anything
+  return csv.map(({ [col]: _, ...rest }) => rest);
+}
+
+function CSV_formatFees(csv)
+{
+  // Finds all rows that match the pattern and gives them a new format
+  const opType = "fee";
+  const regex = /^Costes de transacción y\/o externos de DEGIRO$/i; // /i flag makes it case insensitive
+  // Loop csv and return a new one with formatted matching rows
+  return csv.map( row => {
+    // regex.test is true or false only
+    const match = regex.test(row[IN_CSV_HEADER_DESCRIPCION]);
+    // Don't touch rows that don't match
+    if (match !== true)
+      return {...row};
+    // If row has already been marked and it matches again it's an issue
+    if (row[HEADER_MARKED_TAG] === true) {
+      console.error(`${opType} matched an already marked row of type ${row["type"]}. Skipping...`);
+      return {...row};
+    }
+    // Save fields
+    return {
+      [HEADER_DATE]: row[HEADER_DATE], // Mandatory
+      [HEADER_TRANSACTION_TYPE]: opType, // Mandatory
+      [HEADER_MARKED_TAG]: true, // Mandatory
+      [HEADER_AMOUNT]: Number(row[IN_CSV_HEADER_COL_9].replace(",", ".")),
+      [HEADER_CURRENCY]: row[IN_CSV_HEADER_VARIACION],
+    };
+  });
+}
+
+function CSV_formatOperations(/*const*/ csv)
+{
+  // Finds all rows that match the pattern and gives them a new format
+  const opType = "operation";
+  const regex = /^(\w+)\s+(\d+)\s+(.*?)@([\d,]+)\s+(\w+)\s+\((.+)\)$/i; // /i flag makes it case insensitive
+  // Loop csv and return a new one with formatted matching rows
+  return csv.map( row => {
+    // Match should be:
+    //[
+    //  'Compra 10 ServiceNow Inc@12,345 USD (US01234P5678)',
+    //  'Compra',
+    //  '10',
+    //  'ServiceNow Inc',
+    //  '12,345',
+    //  'USD',
+    //  'US01234P5678'
+    //]
+    const match = row[IN_CSV_HEADER_DESCRIPCION].match(regex);
+    // Don't touch rows that don't match
+    if (match === null)
+      return {...row};
+    // If row has already been marked and it matches again it's an issue
+    if (row[HEADER_MARKED_TAG] === true) {
+      console.error(`${opType} matched an already marked row of type ${row["type"]}. Skipping...`);
+      return {...row};
+    }
+    // If it's a sale, make share count negative
+    let shareCount = Number(match[2].replace(",", "."));
+    if (match[1].toLowerCase() === "venta")
+      shareCount *= -1;
+    // Save fields
+    return {
+      [HEADER_DATE]: row[HEADER_DATE], // Mandatory
+      [HEADER_TRANSACTION_TYPE]: opType, // Mandatory
+      [HEADER_MARKED_TAG]: true, // Mandatory
+      [HEADER_SHARE_COUNT]: shareCount,
+      [HEADER_STOCK_NAME]: match[3],
+      [HEADER_ENTRY_PRICE]: Number(match[4].replace(",", ".")),
+      [HEADER_CURRENCY]: match[5],
+      [HEADER_ISIN]: match[6]
+    };
+  });
 }
 
 function main()
